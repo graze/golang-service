69c5e1dd6ad448dbdfef9162a862830590912529
Harry Bragg <harry.bragg@graze.com>
2016-10-12 14:32:42 +0100

    update doc.go

--- doc.go
+++ doc.go
@@ -9,13 +9,13 @@
 // link:    https://github.com/graze/golang-service
 
 /*
-Package golang-service is a set of packages that provide many tools for helping create services in golang
+golang-service is a set of packages to help with creating services using golang for logging and testing
 
 golang-service contains the following packages:
 
 The logging package provides a set of http.Handler logging handlers to write specific logs about requests
 
-The logging/handlers package provides a set of handlers that read from ENV variables to create the logging handlers
+The handlers package provides a set of handlers that read from ENV variables to create the logging handlers
 
 The nettest package provides a set of helpers for use when testing networks
 */
